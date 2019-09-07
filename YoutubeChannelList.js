f9d2794643025b446868f847b80ec3b26b8f4588
jbhouse <jbmoorhouse1@gmail.com>
2019-09-06 21:00:05 -0400

    develop: update channel list formatting and added new entry

--- YoutubeChannelList.js
+++ YoutubeChannelList.js
@@ -1,6 +1,27 @@
 const channelList = {
-    tech: ["https://www.youtube.com/user/TheVerge/videos", "https://www.youtube.com/user/lexfridman/videos", "https://www.youtube.com/user/unboxtherapy/videos"],
-    learning: ["https://www.youtube.com/user/JordanPetersonVideos/videos", "https://www.youtube.com/user/TEDxTalks/videos"],
-    entertainment: ["https://www.youtube.com/user/Aphromoo/videos", "https://www.youtube.com/channel/UCj1NEvOGuhf9UoknPBF3pbg/videos", "https://www.youtube.com/channel/UCO4NTLDIYKEV_sEwjZq7ghw/videos", "https://www.youtube.com/channel/UCXNd7DlQsKT9-nP_CKTNisQ/videos", "https://www.youtube.com/channel/UCBNcq7Kdw1F9LQ1zRpjeeWw/videos", "https://www.youtube.com/user/MobileTechReview/videos", "https://www.youtube.com/channel/UC01zP3AZBRBDRh-lbPFV5jA/videos"],
-    softwareDevelopment: ["https://www.youtube.com/channel/UCtxCXg-UvSnTKPOzLH4wJaQ/videos"]
+    tech: [
+        "https://www.youtube.com/user/Techquickie/videos",
+        "https://www.youtube.com/user/TheVerge/videos",
+        "https://www.youtube.com/user/lexfridman/videos",
+        "https://www.youtube.com/user/unboxtherapy/videos"
+    ],
+
+    learning: [
+        "https://www.youtube.com/user/JordanPetersonVideos/videos",
+        "https://www.youtube.com/user/TEDxTalks/videos"
+    ],
+
+    entertainment: [
+        "https://www.youtube.com/user/Aphromoo/videos",
+        "https://www.youtube.com/channel/UCj1NEvOGuhf9UoknPBF3pbg/videos",
+        "https://www.youtube.com/channel/UCO4NTLDIYKEV_sEwjZq7ghw/videos",
+        "https://www.youtube.com/channel/UCXNd7DlQsKT9-nP_CKTNisQ/videos",
+        "https://www.youtube.com/channel/UCBNcq7Kdw1F9LQ1zRpjeeWw/videos",
+        "https://www.youtube.com/user/MobileTechReview/videos",
+        "https://www.youtube.com/channel/UC01zP3AZBRBDRh-lbPFV5jA/videos"
+    ],
+
+    softwareDevelopment: [
+        "https://www.youtube.com/channel/UCtxCXg-UvSnTKPOzLH4wJaQ/videos"
+    ]
 }
