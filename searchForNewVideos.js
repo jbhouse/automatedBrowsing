90b685493a82bac5b40cadb5e17e1f899e67202a
jbhouse <jbmoorhouse1@gmail.com>
2019-09-07 22:14:28 -0400

    develop: finished POC that opens a new tab for each recent video for each channel I am interested in

--- searchForNewVideos.js
+++ searchForNewVideos.js
@@ -8,68 +8,49 @@ const puppeteer = require('puppeteer');
         headless: false // Puppeteer is 'headless' by default.
     });
 
-    const page = await browser.newPage();
-    // let pageUrl = 'https://www.youtube.com/user/Techquickie/videos';
-    let pageUrl = 'https://www.youtube.com/channel/UCcefcZRL2oaA_uBNeo5UOWg/videos';
+    Object.keys(channels.videoPages).forEach(topic => {
+        let allChannelsInCategoryArray = channels.videoPages[topic]
+        allChannelsInCategoryArray.forEach(async (channelUrl) => {
 
-    await page.goto(pageUrl, {
+            // for each channel we go to their video page
+            openNewPageToUrl(browser, channelUrl).then(async (page) => {
+                let recentVideoCount = await page.evaluate(() => {
+                    // we then check for recent videos
+                    let recentVideos = Array.from(document.querySelectorAll('#metadata-line'))
+                        .filter(thumbNail => thumbNail.textContent.includes('hours') || thumbNail.textContent.includes('day ago'));
+                    // if there are any, we click the link to the first one and return the total amount of recent videos
+                    if (recentVideos.length > 0) {
+                        recentVideos[0].click();
+                    }
+                    return recentVideos.length;
+                });
+                // if there is more than one new video, we go and open that channel again, and click the links on the rest of the videos
+                // this is pretty inefficient and annoying, but for now, it does the job. improve this later
+                if (recentVideoCount > 1) {
+                    for (let i = 1; i < recentVideoCount; i++) {
+                        let newPage = await openNewPageToUrl(browser, channelUrl);
+                        newPage.evaluate((i) => {
+                            let recentVideos = Array.from(document.querySelectorAll('#metadata-line'))
+                                .filter(thumbNail => thumbNail.textContent.includes('hours') || thumbNail.textContent.includes('day ago'));
+
+                            recentVideos[i].click();
+                        }, i);
+
+                    }
+                }
+            })
+        })
+    })
+})();
+
+
+
+async function openNewPageToUrl(browser, urlString) {
+    const page = await browser.newPage();
+
+    await page.goto(urlString, {
         waitUntil: 'networkidle0' // 'networkidle0' is very useful for SPAs.
     });
 
-    page.evaluate(() => {
-        const tds = Array.from(document.querySelectorAll('#metadata-line'));
-        // tds.filter(td => td.textContent.split('views')[1].includes('hours') || td.textContent.split('views')[1].includes('day ago'))[0].click();
-        // tds.filter(td => td.textContent.split('views')[1].includes('hours') || td.textContent.split('views')[1].includes('day ago')).forEach(newVideo => {
-        //     newVideo.click()
-        // })
-        return tds
-            .filter(
-                td => td.textContent.split('views')[1].includes('hours') ||
-                    td.textContent.split('views')[1].includes('day ago'))
-            .map(td => td.textContent)
-    }).then(idk => {
-        console.log("yes");
-        console.log(idk);
-        // likely need to find a way to take the relevant textContent and open a new page for each video
-    });
-
-
-
-    // Object.keys(channels.videoPages).forEach(topic => {
-    //     let allChannelsInCategoryArray = channels.videoPages[topic]
-    //     allChannelsInCategoryArray.forEach(async (channelUrl) => {
-    //         let page = await browser.newPage();
-    //         let pageUrl = channelUrl;
-
-    //         await page.goto(pageUrl, {
-    //             waitUntil: 'networkidle0' // 'networkidle0' is very useful for SPAs.
-    //         });
-    //     })
-    // })
-
-
-
-
-    // let mostSearchedList = await page.evaluate(() => {
-    //     let objectList = document.querySelectorAll('.js-most-searched .home__list-item');
-    //     let mostSearched = [];
-
-    //     objectList.forEach((item) => {
-    //         let child = item.firstChild;
-    //         let title = child.innerText;
-    //         let href = child.href;
-
-    //         mostSearched.push(title + ' - ' + href);
-    //     });
-
-    //     return mostSearched;
-    // });
-
-    // fs.writeFile("mostSearched.txt", mostSearchedList, function (err) {
-    //     if (err) {
-    //         return console.log(err);
-    //     }
-
-    //     console.log("The file was saved!");
-    // });
-})();
+    return page;
+}
