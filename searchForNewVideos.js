5db6841d5d5f7314344fa2be0f59c8694fdb8e68
jbhouse <jbmoorhouse1@gmail.com>
2019-09-08 17:19:39 -0400

    develop: added debugging file which will not be checked in to git, also updated viewport size of pages to more fully fit the screen size

--- searchForNewVideos.js
+++ searchForNewVideos.js
@@ -3,14 +3,27 @@ const puppeteer = require('puppeteer');
 
 async function openAllVideos(categories) {
     const browser = await puppeteer.launch({
-        headless: false // Puppeteer is 'headless' by default.
+        headless: false, args: ['--start-maximized'] // Puppeteer is 'headless' by default.
     });
+
 
     categories.forEach(topic => {
         channels.videoPages[topic].forEach(async (channelUrl) => {
             // for each channel we go to their video page
             openNewPageToUrl(browser, channelUrl).then(async (page) => {
+
+                // set viewport to be set equivalent to the screen size roughly offset to the margins of the chrome browser + bottom toolbar
+                let screenSize = await page.evaluate(() => {
+                    return {
+                        width: window.screen.width,
+                        height: window.screen.height
+                    };
+                })
+                await page.setViewport({ width: screenSize.width - 5, height: screenSize.height - 160 });
+
                 let recentVideoCount = await page.evaluate(() => {
+                    console.log("width: " + document.width);
+                    console.log("height: " + document.height);
                     // we then check for recent videos
                     let recentVideos = Array.from(document.querySelectorAll('#metadata-line'))
                         .filter(thumbNail => thumbNail.textContent.includes('minute') || thumbNail.textContent.includes('hour') || thumbNail.textContent.includes('day ago'));
@@ -23,6 +36,16 @@ async function openAllVideos(categories) {
                 if (recentVideoCount > 1) {
                     for (let i = 1; i < recentVideoCount; i++) {
                         let newPage = await openNewPageToUrl(browser, channelUrl);
+
+                        // set viewport to be set equivalent to the screen size roughly offset to the margins of the chrome browser + bottom toolbar
+                        let screenSize = await page.evaluate(() => {
+                            return {
+                                width: window.screen.width,
+                                height: window.screen.height
+                            };
+                        })
+                        await page.setViewport({ width: screenSize.width - 5, height: screenSize.height - 160 });
+
                         newPage.evaluate((i) => {
                             let recentVideos = Array.from(document.querySelectorAll('#metadata-line'))
                                 .filter(thumbNail => thumbNail.textContent.includes('hours') || thumbNail.textContent.includes('day ago'));
@@ -45,6 +68,8 @@ async function openNewPageToUrl(browser, urlString) {
     await page.goto(urlString, {
         waitUntil: 'networkidle0' // 'networkidle0' is very useful for SPAs.
     });
+
+    await page.setViewport({ width: 1920, height: 920 });
 
     return page;
 }
