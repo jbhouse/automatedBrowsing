494394f5239d1828972c25e4575bf542d1f68f2a
jbhouse <jbmoorhouse1@gmail.com>
2024-05-03 12:39:52 -0600

    master : happy with this now

--- searchForNewVideos.js
+++ searchForNewVideos.js
@@ -8,29 +8,40 @@ async function openAllVideos() {
         headless: true//, args: ['--start-maximized'] // Puppeteer is 'headless' by default.
     });
 
-    for(i = 0; i < channels.creators.length; i++) {
-        let creator = channels.creators[i];
+    const currentCreators = channels.creators.filter(channel => process.argv.includes(channel.channelName))
+
+    for(i = 0; i < currentCreators.length; i++) {
+        let creator = currentCreators[i];
         await openNewPageToUrl(browser, creator.channelUrl)
         .then(async page => {
             await page.waitForSelector('#video-title-link');
             let videosToDownload = await page.evaluate(async (creator) => {
                 return Array.from(document.querySelectorAll('#video-title-link'))
-                    .filter(video => !creator.videos.includes(video.innerText))
+                    .filter(video => !creator.videos.includes(video.href))
                     .map(vid => ({
                         title: vid.innerText,
                         link: vid.href
                     }));
             }, creator)
             videosToDownload.forEach(async vid => {
-                console.log(`downloading video: '${vid.title}' for creator: '${creator.channelName}'`)
-                exec(`mkdir -p ~/Documents/videos/${creator.channelName} && cd ~/Documents/videos/${creator.channelName} && youtube-dl ${vid.link}`, (err, stdout, stderr) => {
+                console.log(`downloading video: '${vid.title}' for creator: '${creator.channelName}'`);
+                exec(`mkdir -p ~/Documents/videos/${creator.channelName} && cd ~/Documents/videos/${creator.channelName} && youtube-dl -f 140 ${vid.link}`, (err, stdout, stderr) => {
                     if (err) {
-                      console.log(`err: ${err}`)
-                      return;
+                        console.log("--------------");
+                        console.log(`mkdir -p ~/Documents/videos/${creator.channelName} && cd ~/Documents/videos/${creator.channelName} && youtube-dl -f 140 ${vid.link}`);
+                        console.log("--------------");
+                        console.log(`err: ${err}`)
+                        return;
                     } else {
-                        console.log(`stdout: ${stdout}`);
-                        console.log(`stderr: ${stderr}`);
-                        creator.videos.push(vid.title);
+                        console.log(`stderr: ${stderr}`)
+                        console.log(`${stdout}`);
+                        console.log("==================");
+                        console.log(`downloaded ${vid.title}`);
+                        console.log("==================");
+                        console.log(`from link ${vid.link}`);
+                        console.log("==================");
+                        creator.videos.push(vid.link);
+                        fs.writeFileSync(process.argv[1] + 'on', JSON.stringify(channels, null, 2));
                     }
                   });
             })
@@ -38,9 +49,6 @@ async function openAllVideos() {
         .catch(exception => {
             console.log(`creator: ${creator} had exception ${exception}`);
         })
-        if(i == channels.creators.length - 1) {
-            fs.writeFileSync(process.argv[1] + 'on', JSON.stringify(channels, null, 2));
-        } 
     }
     
 }
